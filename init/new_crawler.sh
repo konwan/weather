57085b82e51e2a4e49e8d27047082b9769045991
konwan <tkw1004@gmail.com>
2017-01-16 16:13:59 +0800

    [Fix] - change path

--- init/new_crawler.sh
+++ init/new_crawler.sh
@@ -3,8 +3,10 @@
 # sh new_crawler.sh
 ##################################
 
-now=$(pwd)
+
 prj=spiderman
+depuser=$(whoami)
+now=/opt/dev_${prj}
 user=weacrawler
 host=$(hostname)
 dt=$(date +'%Y-%m-%d-%H:%M:%S')
@@ -15,34 +17,51 @@ showinfo(){
 }
 
 setenv(){
-    showinfo "Add new spider"
-    sudo adduser ${user}
-    sudo adduser ${user} sudo
+    id ${user}
+    res=$?
 
-    showinfo "Setup ssh channel"
-    sudo cp ~/.ssh/id_rsa.pub  /home/${user}/.ssh/authorized_keys
-    sudo chmod 600 /home/${user}/.ssh/authorized_keys
-    sudo chown ${user}:${user} /home/${user}/.ssh/authorized_keys
+    if [ ${res} -ne 0 ];then
+        showinfo "Add new spider"
+        sudo adduser ${user}
+        sudo adduser ${user} sudo
+
+        showinfo "Setup ssh channel"
+        sudo mkdir /home/${user}/.ssh && sudo chown ${user}:${user} /home/${user}/.ssh && sudo chmod 600 /home/${user}/.ssh
+        sudo cp ~/.ssh/id_rsa.pub  /home/${user}/.ssh/authorized_keys
+        sudo chmod 600 /home/${user}/.ssh/authorized_keys
+        sudo chown ${user}:${user} /home/${user}/.ssh/authorized_keys
+    else
+        showinfo "${user} already exist"
+    fi
 }
 
 getcode(){
+    if [ ! -d "${now}" ]; then
+        sudo mkdir ${now}
+        sudo chown ${devuser}:${devuser} ${now}
+    fi
+    cd ${now}
     showinfo "Get latest spider code"
-    mkdir code
-    cd ${now}/code
-    git clone git@bitbucket.org:goming/${prj}.git
+    git clone -b weather git@bitbucket.org:goming/${prj}.git
     sudo cp -R ${prj} /home/${user}
     sudo chown -R ${user}:${user} /home/${user}/${prj}
-    cd ..
-    sudo rm -rf code
 }
 
 startspider(){
     showinfo "Deploy spider env"
     ssh -t ${user}@${host} "whoami; cd ${prj}; sh deploy_weather.sh; crontab -l;"
-    sed -i "s|/opt/upbq.sh|${now}/upbq.sh|g" upbq.cron
-    crontab upbq.cron
+    sed -i "s|/opt/upbq.sh|${now}/${prj}/init/upbq.sh|g" ${now}/${prj}/init/upbq.cron
+    crontab ${now}/${prj}/init/upbq.cron
 }
 
+clearuser(){
+    sudo rm /var/spool/cron/crontabs/${depuser}
+    sudo deluser ${user}
+    sudo rm -rf /home/${user}
+    sudo rm -rf ${now}/${prj}
+}
+
+clearuser
 setenv
 getcode
 startspider
